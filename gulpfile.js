dca16f58210fa42595c9e588cfc13f6c36cf7de4
Elia Grachev <egrachev@yandex.ru>
2018-05-23 17:53:40 +0300

    Just second commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+
+/* -------- Server -------- */
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            port: 9000,
+            baseDir: "build"
+        }
+    });
+
+    gulp.watch('build/**/*').on('change', browserSync.reload);
+});
+
+
+/* -------- Pug compile -------- */
+gulp.task('templates:compile', function buildHTML() {
+    return gulp.src('source/template/index.pug')
+        .pipe(pug({
+            // Your options in here.
+            pretty: true
+        }))
+        .pipe(gulp.dest('build'))
+});
